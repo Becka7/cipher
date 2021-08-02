081d207037204ee88623222b3a98f0d830fac6f5
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-08-02 16:35:22 +0300

    created app class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,9 @@
 //
 //
-public class App {
-
-
-    public static void main(String[] args) {
-
-    }
-}
+//public class App {
+//
+//
+//    public static void main(String[] args) {
+//
+//    }
+//}
