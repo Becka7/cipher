236eaf57081e5972cb6222033da0d3db174bf7e4
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-08-02 16:15:57 +0300

    worte the deciphering algorithm

--- src/main/java/Decipher.java
+++ src/main/java/Decipher.java
@@ -5,21 +5,21 @@ public class Decipher {
     {
         StringBuffer result= new StringBuffer();
 
-//        for (int i=0; i<text.length(); i++)
-//        {
-//            if (Character.isUpperCase(text.charAt(i)))
-//            {
-//                char ch = (char)(((int)text.charAt(i) - shift - 65) % 26 + 65);
-//                result.append(ch);
-//            }
-//            else
-//            {
-//                char ch = (char)(((int)text.charAt(i) - shift - 97) % 26 + 97);
-//                result.append(ch);
-//            }
-//        }
-//        return result;
-//    }
+        for (int i=0; i<text.length(); i++)
+        {
+            if (Character.isUpperCase(text.charAt(i)))
+            {
+                char ch = (char)(((int)text.charAt(i) - shift - 65) % 26 + 65);
+                result.append(ch);
+            }
+            else
+            {
+                char ch = (char)(((int)text.charAt(i) - shift - 97) % 26 + 97);
+                result.append(ch);
+            }
+        }
+        return result;
+    }
 //
 //
 //    public static void main(String[] args)
